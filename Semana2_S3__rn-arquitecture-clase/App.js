c45a4c105dbae830f4d28780c9eb83e7c0b55c6a
LAB-USR-LNORTE <LAB-USR-LNORTE@grupoutp.net>
2026-08-17 15:27:29 -0500

    Semana 2 [Sesion 3]: Subiendo parte final del ejercició

--- Semana2_S3__rn-arquitecture-clase/App.js
+++ Semana2_S3__rn-arquitecture-clase/App.js
@@ -7,24 +7,25 @@ import StyleDemo from "./src/screens/StyleDemo";
 
 export default function App() {
   return (
-    <>
+    <div style={styles.divi}>
       <SafeAreaProvider style={styles.safe}>
         <View style={styles.container}>
           <ProfileCard />
           <ProfileCardURL
             image="https://http.cat/images/102.jpg"
             name="Gatito HTTP Status 102 "
-            description="Un gatito que nos dice el codigo de error http"
+            description="Un gatito que nos dice el codigo de error http 102"
           />
         </View>
       </SafeAreaProvider>
-      <GridDemo />
       <StyleDemo />
-    </>
+      <GridDemo />
+    </div>
   );
 }
 
 const styles = StyleSheet.create({
+  divi: { overflow: "scroll", flex: 1, justifyContent: "center" },
   safe: { flex: 1, backgroundColor: "#F1F5F9" },
   container: {
     flex: 1,
